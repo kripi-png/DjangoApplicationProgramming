d4392bacb9e7977623954a9a597a4992805e8aeb
Dragiln <78900645+Dragiln@users.noreplay.github.com>
2021-12-02 12:47:46 +0200

    fixed the mistake

--- board_game/models.py
+++ board_game/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Boardgame(models.Model):
     #A game user can borrow
-    name = models.Charfield(max_length=200)
+    name = models.CharField(max_length=200)
 
     date_added = models.DateTimeField(auto_now_add=True)
 
