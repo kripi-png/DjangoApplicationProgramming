426ada0779a31c7935696bf93c6cfa93d361b318
Dragiln <78900645+Dragiln@users.noreplay.github.com>
2021-12-02 13:12:53 +0200

    created urls for board_game and modified url from BoardGame

---
+++ board_game/urls.py
@@ -0,0 +1,12 @@
+# Define URL pattern for board_game
+
+from django.urls import path
+
+from . import views
+
+app_name = 'board_game'
+
+urlpatterns =[
+    # Home page
+    path('', views.index, name='index'),
+]
