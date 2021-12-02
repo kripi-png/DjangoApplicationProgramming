121cca3ce31d0fbd2542cd1e4846f89295d8e6df
Dragiln <78900645+Dragiln@users.noreplay.github.com>
2021-12-02 12:45:48 +0200

    defined boardgame model

--- board_game/models.py
+++ board_game/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Boardgame(models.Model):
+    #A game user can borrow
+    name = models.Charfield(max_length=200)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    date_modifier = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        #return a string representation of the models
+        return self.name
