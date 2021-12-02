414170422c06d7ab54ff02a1771d466fb8a03654
kripi-png <66375160+kripi-png@users.noreply.github.com>
2021-12-02 13:22:13 +0200

    changed comments to docstrings; cleaning

--- board_game/models.py
+++ board_game/models.py
@@ -1,25 +1,21 @@
 from django.db import models
 
 class BoardGame(models.Model):
-    #A game user can borrow
+    """A game user can borrow."""
     name = models.CharField(max_length=200)
-
     date_added = models.DateTimeField(auto_now_add=True)
-
     date_modifier = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        #return a string representation of the models
+        """Return a string representation of the models."""
         return self.name
 
 class Review(models.Model):
-    boardgame = models.ForeignKey(BoardGame, on_delete=models.CASCADE)
+    """A review a user can write for a boardgame."""
+    board_game = models.ForeignKey(BoardGame, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        verbose_name_plural = 'Reviews'
-
     def __str__(self):
-        # Return a string representation of the model
-        return f'{self.text[:50]}...'
+        """Return a string representation of the model."""
+        return f"{self.text[:50]}..."
