87ee9c148e253f07be42499d2558d1b7f66ea4b5
Jurzzyy <78170040+Jurzzyy@users.noreply.github.com>
2021-12-02 13:13:23 +0200

    Made Review model and registered it to admin site

--- board_game/admin.py
+++ board_game/admin.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
 
 # Registering a topic with admin site
-from .models import BoardGame
+from .models import BoardGame, Review
 admin.site.register(BoardGame)
-
-class Review(models.Model):
-    boardgame = models.ForeignKey(BoardGame, on_delete=models.CASCADE)
-    text = models.TextField()
-    date_added = models.DateTimeField(auto_now_Add=True)
-
-    class Meta:
-        verbose_name_plural = 'Reviews'
-
-    def __str__(self):
-        # Return a string representation of the model
-        return f'{self.text[:50]}...'
+admin.site.register(Review)
--- board_game/models.py
+++ board_game/models.py
@@ -11,3 +11,15 @@ class BoardGame(models.Model):
     def __str__(self):
         #return a string representation of the models
         return self.name
+
+class Review(models.Model):
+    boardgame = models.ForeignKey(BoardGame, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_Add=True)
+
+    class Meta:
+        verbose_name_plural = 'Reviews'
+
+    def __str__(self):
+        # Return a string representation of the model
+        return f'{self.text[:50]}...'
