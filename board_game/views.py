5255ea1c02d37d8f144e15ad721f2d56534ca3c4
kripi-png <66375160+kripi-png@users.noreply.github.com>
2021-12-07 15:38:55 +0200

    line breaks for readability

--- board_game/views.py
+++ board_game/views.py
@@ -7,12 +7,14 @@ from .forms import BoardGameForm, ReviewForm
 def index(request):
     # Home page for BoardGame
     return render(request, 'board_game/index.html')
+
 @login_required
 def games(request):
     """Display all games."""
     games = BoardGame.objects.order_by('date_added')
     context = {'games': games}
     return render(request, 'board_game/games.html', context)
+
 @login_required
 def game(request, game_id):
     """Show a single game and all its entries"""
@@ -20,6 +22,7 @@ def game(request, game_id):
     reviews = game.review_set.order_by('-date_added')
     context = {'game': game, 'reviews': reviews}
     return render(request, 'board_game/game.html', context)
+
 @login_required
 def new_game(request):
     """Add a new game"""
@@ -38,6 +41,7 @@ def new_game(request):
     # Display a blank or invalid form
     context = {'form': form}
     return render(request, 'board_game/new_game.html', context)
+
 @login_required
 def new_review(request, game_id):
     """Add a new review for a particular game"""
@@ -57,6 +61,7 @@ def new_review(request, game_id):
     # Display a blank or invalid review
     context = {'game': game, 'form': form}
     return render(request, 'board_game/new_review.html', context)
+
 @login_required
 def edit_review(request, review_id):
     """Edit an existing review."""
