fb5f44c0e60d7d1aaf28412e29a1dc42f32d298d
kripi-png <66375160+kripi-png@users.noreply.github.com>
2021-12-16 14:58:15 +0200

    cleaning the code a bit

--- README.md
+++ README.md
@@ -2,5 +2,3 @@ Topic = BoardGame, Entry = Review
 
 
 ## Todo
-- [ ] how many people have borrowed a game
-- [x] game average score
--- users/views.py
+++ users/views.py
@@ -13,7 +13,7 @@ def register(request):
         form = UserCreationForm(data=request.POST)
         if form.is_valid():
             new_user = form.save()
-        # Log the user in and then redirect to home page.
+            # Log the user in and then redirect to home page.
             login(request, new_user)
             return redirect('board_game:index')
     # Display a blank or invalid form.
